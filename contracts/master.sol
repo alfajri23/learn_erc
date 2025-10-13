12dd67127383c5658a02535c21a5dabc2cf52730
alfajri23 <feri.alfajri@gmail.com>
2025-10-13 15:29:39 +0700

    add library

---
+++ contracts/master.sol
@@ -0,0 +1,31 @@
+// FETokenOz.sol
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// Impor kontrak standar yang sudah diaudit
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.0/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.0/contracts/access/Ownable.sol";
+
+// Kontrak mewarisi semua fungsi dasar ERC20 dan Ownable
+contract FETokenOz is ERC20, Ownable {
+
+    constructor(uint256 initialSupply) 
+        ERC20("FE Token", "FE") // Nama dan Simbol
+        Ownable()
+    {
+        uint256 supplyWithDecimals = initialSupply * 10**decimals(); 
+
+        // Fungsi internal OZ untuk mencetak token awal
+        _mint(msg.sender, supplyWithDecimals);
+    }
+    
+    // Fungsi untuk menambah suplai (Minting) - Dibatasi hanya untuk pemilik
+    function mint(address to, uint256 amount) public onlyOwner {
+        require(to != address(0), "ERC20: mint ke alamat nol ditolak");
+        _mint(to, amount);
+    }
+    
+    // Semua fungsi wajib lainnya (transfer, balanceOf, approve, dll.) 
+    // sudah diimplementasikan dan aman secara otomatis.
+}
